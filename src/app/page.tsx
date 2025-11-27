9fce03f02b3797c538c12b414fc4892c703615fc
BrightNtt <nitiratnongyai@gmail.com>
2025-11-27 23:47:56 +0700

    feat[app]: add background image to hero-section

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "../components/heroSection";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="flex min-h-screen w-full flex-col items-center justify-between bg-white">
+        <HeroSection />
+      </main>
+    </div>
+  );
+}
---
+++ src/components/heroSection.tsx
@@ -0,0 +1,14 @@
+const HeroSection = () => {
+  return (
+    <div
+      className="bg-cover bg-center w-full h-screen"
+      style={{
+        backgroundImage: "url('/images/tennis-hero-bg.jpg')",
+      }}
+    >
+      Hello there
+    </div>
+  );
+};
+
+export default HeroSection;
