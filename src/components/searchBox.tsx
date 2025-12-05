fd442354946ed95b1c9b9e55027f4a373f8222e9
BrightNtt <nitiratnongyai@gmail.com>
2025-12-06 00:58:47 +0700

    chore[app]: add icon to searchBox component

--- src/components/searchBox.tsx
+++ src/components/searchBox.tsx
@@ -1,11 +1,14 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
 const SearchBox = () => {
   return (
-    <div className="w-full max-w-4xl">
+    <div className="flex flex-row justify-between items-center gap-2 w-full h-12 max-w-4xl px-5 py-2 border border-gray-300 rounded-full text-base text-white bg-gray-300/40">
       <input
         type="text"
         placeholder="What do you feel like playing today?"
-        className="w-full h-12 px-5 py-2 border border-gray-300 rounded-full text-base text-white bg-gray-300/40 outline-none"
+        className="w-full border-r border-gray-300 text-base text-white outline-none"
       />
+      <MagnifyingGlassIcon className="h-5 w-5 text-gray-300" />
     </div>
   );
 };
