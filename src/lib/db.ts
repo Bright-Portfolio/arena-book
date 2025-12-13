6c4f19ceeba7f59107ce5861a44d026ced68a894
BrightNtt <nitiratnongyai@gmail.com>
2025-12-14 01:23:43 +0700

    create db.ts

---
+++ src/lib/db.ts
@@ -0,0 +1,23 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+//test connection
+export async function testConnection() {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT NOW()");
+    console.log("database connection successfully");
+    console.log("server time", result.rows[0].now);
+
+    return true;
+  } catch (error) {
+    console.error("database connection failed:", error);
+
+    return false;
+  }
+}
+
+export default pool;
