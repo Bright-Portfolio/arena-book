15217402516538e458526ac61ce4595ac48c1341
BrightNtt <nitiratnongyai@gmail.com>
2025-12-20 15:18:09 +0700

    feat[repositories]: create findUserByEmail and CreateUser func

--- src/lib/repositories/user.repo.ts
+++ src/lib/repositories/user.repo.ts
@@ -1,17 +1,40 @@
 import pool from "@/src/lib/db";
 
+type UserRole = "user" | "owner";
+type AuthProviderType = "credentials" | "google";
+
 export interface UserRow {
-    id: Number;
-    email: string;
-    password: string | null;
-    name: string | null;
-    role: 
-    auth_provider: string
-}
-
-export interface RegisterInput {
+  id: number;
   email: string;
-
+  password: string | null;
+  name: string | null;
+  role: UserRole;
+  auth_provider: AuthProviderType;
 }
 
-export async function findUserById() {}
+export async function findUserByEmail(email: string): Promise<UserRow | null> {
+  // Find existing user
+  const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+
+  if (!result) return null;
+
+  return result.rows[0];
+}
+
+export async function createUser(data: {
+  email: string;
+  password?: string;
+  name?: string;
+  authProvider: AuthProviderType;
+}): Promise<UserRow> {
+  const result = await pool.query(
+    `INSERT INTO users (emai, password, name, role, auth_provider)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING`,
+    [data.email, data.password, data.name, "user", data.authProvider]
+  );
+
+  return result.rows[0];
+}
