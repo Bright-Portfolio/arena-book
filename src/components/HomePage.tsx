ed874694c0bf1fb1db757340b227de889b75f228
BrightNtt <nitiratnongyai@gmail.com>
2025-12-22 01:38:19 +0700

    feat[components]: implement signup/signin form in navbar but not done yet

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 // import SearchBox from "./searchBox";
 import Navbar from "./navbar";
@@ -10,7 +11,7 @@ const HomePage = () => {
   const [showAuthModal, setShowAuthModal] = useState(false);
   return (
     <div className="relative flex flex-col items-center justify-between w-full h-screen pt-14 pb-4 pr-4 pl-4">
-      <Navbar />
+      <Navbar onSignIn={() => setShowAuthModal(true)} />
       {/* Slide Banner Background image */}
       <div className="relative z-10 w-full h-[600px] rounded-lg overflow-hidden bg-red-100">
         <Image
@@ -23,12 +24,18 @@ const HomePage = () => {
         />
       </div>
       {/* Temp booking button */}
+
       <button
+        type="button"
         onClick={() => setShowAuthModal(true)}
-        className="p-1 rounded-full text-white bg-black cursor-pointer"
+        className="inline-flex flex-row items-center gap-2 px-2 py-1.5 border border-gray-300 rounded-full text-white bg-black cursor-pointer"
       >
         Book Now
+        <div className="p-2 rounded-full bg-white">
+          <ArrowUpRightIcon className="w-3 h-3 stroke-2 text-black" />
+        </div>
       </button>
+
       {/* <SearchBox /> */}
       <div>Search result here</div>
       <AuthModal
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,17 @@
-import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
+"use client";
 
-const Navbar = () => {
+import { FC } from "react";
+import { useSession } from "next-auth/react";
+
+interface NavbarProps {
+  onSignIn: () => void;
+}
+
+const Navbar: FC<NavbarProps> = ({ onSignIn }) => {
+  const { data: session } = useSession();
+
   return (
-    <nav className="fixed inset-0 z-50 flex flex-row mx-auto px-4 py-1 w-full h-14 bg-transparent items-center justify-between">
+    <nav className="fixed top-0 z-50 flex flex-row mx-auto px-4 py-3 w-full bg-transparent items-center justify-between">
       {/* Left side navigation */}
       <div className="flex justify-start items-center gap-2 w-full text-black">
         <button className="px-2 py-1.5 border border-gray-300 rounded-full cursor-pointer hover:bg-white transition">
@@ -14,15 +23,17 @@ const Navbar = () => {
       </div>
       {/* Right side navigation */}
       <div className="flex justify-end items-center w-full">
-        <button
-          type="button"
-          className="inline-flex flex-row items-center gap-2 px-2 py-1.5 border border-gray-300 rounded-full text-white bg-black cursor-pointer"
-        >
-          Book Now
-          <div className="p-2 rounded-full bg-white">
-            <ArrowUpRightIcon className="w-3 h-3 stroke-2 text-black" />
-          </div>
-        </button>
+        {session ? (
+          <div>{session.user?.name}</div>
+        ) : (
+          <button
+            type="button"
+            onClick={onSignIn}
+            className="inline-flex flex-row items-center gap-2 px-2 py-1.5 border border-gray-300 rounded-full text-white bg-black cursor-pointer"
+          >
+            Sign In
+          </button>
+        )}
       </div>
     </nav>
   );
