7bcbcb9c2e795fb9b10be472857938e91f6c9d67
BrightNtt <nitiratnongyai@gmail.com>
2026-01-01 17:24:47 +0700

    chore[components]: add rotate animate to ComboboxButton

--- src/components/features/company-register/PhoneInput.tsx
+++ src/components/features/company-register/PhoneInput.tsx
@@ -119,50 +119,58 @@ export const PhoneInput: FC<PhoneInputProps> = ({
           onChange={handleCountryChange}
           onClose={() => setQuery("")}
         >
-          <div className="relative">
-            <ComboboxButton className="flex justify-between items-center gap-1 px-2 py-1 border border-gray-300 border-r-0 text-sm text-nowrap rounded-lg rounded-r-none outline-none cursor-pointer data-focus:border-black data-focus:border-r data-active:shadow-inner transition-all duration-75">
-              {selected.flag && <selected.flag className="w-6 h-6" />}
-              <span>+{selected.callingCode}</span>
-              <ChevronDownIcon className="w-4 h-4 text-gray-300" />
-            </ComboboxButton>
-          </div>
+          {({ open }) => (
+            <>
+              <div className="relative">
+                <ComboboxButton className="flex justify-between items-center gap-1 px-2 py-1 border border-gray-300 border-r-0 text-sm text-nowrap rounded-lg rounded-r-none outline-none cursor-pointer data-focus:border-black data-focus:border-r data-active:shadow-inner transition-all duration-75">
+                  {selected.flag && <selected.flag className="w-6 h-6" />}
+                  <span>+{selected.callingCode}</span>
+                  <ChevronDownIcon
+                    className={`w-4 h-4 text-gray-300 transition ${
+                      open ? "rotate-180" : ""
+                    }`}
+                  />
+                </ComboboxButton>
+              </div>
 
-          <ComboboxOptions className="absolute z-10 mt-9 space-y-1 max-h-48 w-40 overflow-y-auto rounded-lg bg-white border border-gray-300 shadow-lg">
-            <div
-              className="sticky top-0 bg-white border-b border-gray-200 p-2"
-              onMouseDown={(e) => e.stopPropagation()}
-            >
-              <div className="flex flex-row justify-start items-center px-2 py-1 w-full border border-gray-300 rounded-md">
-                <MagnifyingGlassIcon className="w-4 h-4 stroke-2 text-gray-400" />
-                <input
-                  type="text"
-                  className="w-full px-2 py-1 outline-none text-sm"
-                  placeholder="Search..."
-                  value={query}
-                  onChange={(e) => setQuery(e.target.value)}
-                />
-              </div>
-            </div>
-            {filteredCountries.length === 0 ? (
-              <div className="px-2 py-1 text-center text-sm text-gray-400">
-                No countries found
-              </div>
-            ) : (
-              filteredCountries.map((country) => (
-                <ComboboxOption
-                  key={country.code}
-                  value={country}
-                  className="group flex flex-row justify-start items-center gap-2 px-2 py-1 cursor-pointer hover:bg-gray-100 data-focus:bg-gray-100"
+              <ComboboxOptions className="absolute z-10 mt-9 space-y-1 max-h-48 w-40 overflow-y-auto rounded-lg bg-white border border-gray-300 shadow-lg">
+                <div
+                  className="sticky top-0 bg-white border-b border-gray-200 p-2"
+                  onMouseDown={(e) => e.stopPropagation()}
                 >
-                  {country.flag && <country.flag className="w-6 h-4" />}
-                  <span className="text-sm flex-1">
-                    ({country.code}) +{country.callingCode}
-                  </span>
-                  <CheckIcon className="invisible  w-3 h-3 stroke-2 text-gray-400 group-data-selected:visible" />
-                </ComboboxOption>
-              ))
-            )}
-          </ComboboxOptions>
+                  <div className="flex flex-row justify-start items-center px-2 py-1 w-full border border-gray-300 rounded-md">
+                    <MagnifyingGlassIcon className="w-4 h-4 stroke-2 text-gray-400" />
+                    <input
+                      type="text"
+                      className="w-full px-2 py-1 outline-none text-sm"
+                      placeholder="Search..."
+                      value={query}
+                      onChange={(e) => setQuery(e.target.value)}
+                    />
+                  </div>
+                </div>
+                {filteredCountries.length === 0 ? (
+                  <div className="px-2 py-1 text-center text-sm text-gray-400">
+                    No countries found
+                  </div>
+                ) : (
+                  filteredCountries.map((country) => (
+                    <ComboboxOption
+                      key={country.code}
+                      value={country}
+                      className="group flex flex-row justify-start items-center gap-2 px-2 py-1 cursor-pointer hover:bg-gray-100 data-focus:bg-gray-100"
+                    >
+                      {country.flag && <country.flag className="w-6 h-4" />}
+                      <span className="text-sm flex-1">
+                        ({country.code}) +{country.callingCode}
+                      </span>
+                      <CheckIcon className="invisible  w-3 h-3 stroke-2 text-gray-400 group-data-selected:visible" />
+                    </ComboboxOption>
+                  ))
+                )}
+              </ComboboxOptions>
+            </>
+          )}
         </Combobox>
         <input
           type="tel"
